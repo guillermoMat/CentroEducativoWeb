1760188f380ee9bd4b2c1551d7273d258992ba8e
guillermo <guillermomathieu@hotmail.com>
2023-08-21 16:45:17 -0300

    1er commit: barra navegacion-index-footer-quienes somos

---
+++ index.php
@@ -0,0 +1,182 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+     <!-- Font Awesome icons (free version)-->
+     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+
+         
+
+</head>
+<body id="page-top">
+
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+    <?php 
+include 'barraNavegacion.php';
+
+?>
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <!-- Main Content -->
+            <div id="content">
+
+                
+                <!-- End of Topbar -->
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+                    <!-- Page Heading -->
+                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                        <h1 class="h3 mb-0 text-gray-800">Inicio</h1>
+                    </div>
+
+                    
+                    
+
+                    <!-- Content Row -->
+
+                    <div class="row">
+
+                       
+
+                        <!-- Pie Chart -->
+                        <div class="col-xl-4 col-lg-5">
+                            <div class="card shadow mb-4">
+                                <!-- Card Header - Dropdown -->
+                                <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
+                                    <div class="dropdown no-arrow">
+                                        <!--- espacio titulo    -->
+                                    </div>
+                                </div>
+                                <!-- Card Body -->
+                                <div class="card-body">
+                                    <div class="chart-pie pt-4 pb-2">
+                                        
+                                        <p>En el momento siguiente, un grito atronador, que parecía emanar de las mismas piedras del suelo, derramó las palabras '¡Viva el Hermano Mayor!' por todas partes. Era un grito estrepitoso y alegre: triste y brutal al mismo tiempo. Era un grito de triunfo y de odio. Y en él parecía vibrar el reconocimiento de la derrota, de la humillación. Era como si hubieran tenido una visión de lo que les aguardaba: las calles amarillentas, las figuras flacas, la boca torcida, el grito horrible.</p>      
+                                    </div>
+                                  <!-- CONTENIDO -->
+
+                                  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Content Row -->
+                    <div class="row">
+
+                        <!-- Content Column -->
+                        <div class="col-lg-6 mb-4">
+                        <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Titulo 1</h6>
+                                    <div class="dropdown no-arrow">
+                                        <!--- espacio titulo    -->
+                                    </div>
+                                </div>
+                            <!-- Project Card Example -->
+                            
+
+                            <p>En el momento siguiente, un grito atronador, que parecía emanar de las mismas piedras del suelo, derramó las palabras '¡Viva el Hermano Mayor!' por todas partes. Era un grito estrepitoso y alegre: triste y brutal al mismo tiempo. Era un grito de triunfo y de odio. Y en él parecía vibrar el reconocimiento de la derrota, de la humillación. Era como si hubieran tenido una visión de lo que les aguardaba: las calles amarillentas, las figuras flacas, la boca torcida, el grito horrible.</p>      
+                            
+
+                        </div>
+
+                        <div class="col-lg-6 mb-4">
+                        <div
+                                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                                    <h6 class="m-0 font-weight-bold text-primary">Titulo 2</h6>
+                                    <div class="dropdown no-arrow">
+                                        <!--- espacio titulo    -->
+                                    </div>
+                                </div>
+                        <p>En el momento siguiente, un grito atronador, que parecía emanar de las mismas piedras del suelo, derramó las palabras '¡Viva el Hermano Mayor!' por todas partes. Era un grito estrepitoso y alegre: triste y brutal al mismo tiempo. Era un grito de triunfo y de odio. Y en él parecía vibrar el reconocimiento de la derrota, de la humillación. Era como si hubieran tenido una visión de lo que les aguardaba: las calles amarillentas, las figuras flacas, la boca torcida, el grito horrible.</p>      
+
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+
+        </div>
+        <!-- End of Content Wrapper -->
+
+    </div>
+    <!-- End of Page Wrapper -->
+
+ <!-- Footer-->
+ <footer class="footer text-center">
+            <div class="container">
+            <h4 style="text-decoration: underline"><b>DEVELOPERS</b></h4>
+            <br/>
+                <div class="row">
+                    <!-- Footer Location-->
+                    
+                  <!-- Footer Location-->
+                  <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Soledad Piedra</h4>
+                        <p class="lead mb-0">
+                        Linkedin
+                            <br />
+                            alguna red social o correo
+                        </p>
+                    </div>
+                    <!-- Footer Location-->
+                    <div class="col-lg-4 mb-5 mb-lg-0">
+                        <h4 class="text-uppercase mb-4">Lucas Encinas</h4>
+                        <p class="lead mb-0">
+                        Linkedin
+                            <br />
+                            alguna red social o correo
+                        </p>
+                    </div>
+                    <!-- Footer Location-->
+                    <div class="col-lg-4">
+                        <h4 class="text-uppercase mb-4">Guillermo Mathieu</h4>
+                        <p class="lead mb-0">
+                        Linkedin
+                            <br />
+                            alguna red social o correo
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+        </footer>
+        <!-- Copyright Section-->
+        <div class="copyright py-4 text-center text-white">
+            <div class="container"><small>Copyright &copy; Your Website 2023</small></div>
+        </div>
+
+
+
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+</html>
---
+++ quienesSomos.php
@@ -0,0 +1,81 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        
+</head>
+<body>
+
+   <!-- Product section-->
+        <section class="py-5">
+  
+  
+            <div class="container px-4 px-lg-5 my-5">
+
+  
+                <div class="row gx-4 gx-lg-5 align-items-center">
+  
+  
+                    <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="img/logo centro educativo.png" alt="..." /></div>
+                    
+                    <div class="col-md-6">
+  
+                        
+                        <h1 class="display-5 fw-bolder">Educar para Transformar</h1>
+  
+                        <div class="fs-5 mb-5">
+  
+                            <!-- <span>subtitulo</span> -->
+                            
+                        </div>
+  
+                        <p class="lead">Inspiramos, desafiamos y empoderamos a todos nuestros alumnos a ser miembros comprometidos y éticos de una comunidad global.
+Para que se conviertan en agentes de cambio conscientes de sí mismos,seguros, innovadores y colaborativos.</p>
+<p class="lead">
+El centro educativo es una institución de gestión privada de alta calidad educativa, ubicada en las afuera de la ciudad de Resistencia. </p>
+                      
+                    </div>
+                </div>
+   
+            </div>
+  
+        </section>
+  <!-- Copyright Section-->
+  <div class="copyright py-4 text-center text-white">
+            <div class="container"><a href="index.php">Volver Inicio</a></div>
+        </div>
+
+
+
+         <!-- Bootstrap core JS-->
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
